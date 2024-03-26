fde17a18dc34f4981d6f2d20f59433d3f032802f
TF-2022 <renolletpierre@yahoo.fr>
2024-03-26 19:33:16 +0100

    Configuration classic ---->

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY') or os.urandom(24)
 
 @app.route('/')
 def index():
     return render_template('index.html')
-
 
 if __name__ == '__main__':
     app.run(
